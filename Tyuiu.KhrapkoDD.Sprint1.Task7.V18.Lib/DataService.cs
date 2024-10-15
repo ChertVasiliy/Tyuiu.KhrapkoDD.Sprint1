80717b74a1e3cb41c2d4cc97e704270d7479278d
Андрей Фабрициус <Tyuiu.FabritsiusAO@outlook.com>
2024-10-15 23:14:33 +0500

    Task 7

--- Tyuiu.KhrapkoDD.Sprint1.Task7.V18.Lib/DataService.cs
+++ Tyuiu.KhrapkoDD.Sprint1.Task7.V18.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.KhrapkoDD.Sprint1.Task7.V18.Lib
 {
-    public class DataService : ISprint1Task7V17
+    public class DataService : ISprint1Task7V18
     {
 
         public double Calculate(double x, double y)
